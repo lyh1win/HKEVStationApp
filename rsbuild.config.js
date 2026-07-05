feadc2b5d77903b1aa3a6add2b79c13a7b8e4b27
lyh1_gpdwin_max <lyh1win@hotmail.com>
2026-07-06 01:38:38 +0800

    base url fix

--- rsbuild.config.js
+++ rsbuild.config.js
@@ -4,6 +4,7 @@ import { pluginReact } from '@rsbuild/plugin-react';
 export default defineConfig({
   plugins: [pluginReact()],
   server: {
+	  base:"/HKEVStationApp",
     // Proxy CLP EV charger API to avoid CORS issues during dev
     proxy: {
       '/api/evcharger': {
